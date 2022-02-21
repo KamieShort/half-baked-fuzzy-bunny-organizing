dbe614bdb87d9e0c483a74a4136476d95d4ed8f9
Kamie Short <kamie.short@gmail.com>
2022-02-21 13:03:03 -0800

    working on displaying families

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,7 @@
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://egdrvheseyhayxbahrri.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnZHJ2aGVzZXloYXl4YmFocnJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ4ODE3MTMsImV4cCI6MTk2MDQ1NzcxM30.9epFpiRNgk8RIyWEaIJUu1on7JQXBpyH2ROvOPGlajE';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -10,6 +11,7 @@ export function getUser() {
 
 export async function getFamilies() {
     // fetch all families and their bunnies
+    const resp = await client.from('loving_families').select(`*, fuzzy_bunnies (*)`);
 
     return checkError(resp);
 }
